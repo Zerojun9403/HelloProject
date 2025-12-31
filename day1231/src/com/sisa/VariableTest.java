ee39720f60179a6bdd394c1d020118f04e09b842
01_KDT <01_KDT@BOOK-UP5F0RS3G7>
2025-12-31 17:12:40 +0900

    1231_단축키정리

---
+++ day1231/src/com/sisa/VariableTest.java
@@ -0,0 +1,17 @@
+package com.sisa;
+
+public class VariableTest {
+	// 변수 테스트 
+	/*
+	 * <이클립스 단축키>
+	 * 인터프린터 실행 : Ctrl +F11
+	 * 자동(단어)완성: Ctrl+Space
+	 * 코드제안 : Ctrl+1
+	 * 주석 : Ctrl + /
+	 * 코드복사 : Ctrl+ alt + ↑↓
+	 * 
+	 * */
+	public static void main(String[] args) {
+		System.out.println("단축키에 대해 알아보아요!!!!1");
+	}
+}
