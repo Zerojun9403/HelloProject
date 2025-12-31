68411396538c322ced2b12486f9e03760fd0632c
01_KDT <01_KDT@BOOK-UP5F0RS3G7>
2025-12-31 17:46:58 +0900

    1231_for문 연습

--- day1231/src/com/sisa/VariableTest.java
+++ day1231/src/com/sisa/VariableTest.java
@@ -29,17 +29,10 @@ public class VariableTest {
 		
 		// 변수를 사용하는 이유 
 		// 동일한 데이터를 두번이상 사용할때 이제는 5단을 출력 하고 싶어요 
-		int dan=5;
-		System.out.println(dan + "*" + 1 + "=" + dan*1);
-		System.out.println(dan + "*" + 2 + "=" + dan*2);
-		System.out.println(dan + "*" + 3 + "=" + dan*3);
-		System.out.println(dan + "*" + 4 + "=" + dan*4);
-		System.out.println(dan + "*" + 5 + "=" + dan*5);
-		System.out.println(dan + "*" + 6 + "=" + dan*6);
-		System.out.println(dan + "*" + 7 + "=" + dan*7);
-		System.out.println(dan + "*" + 8 + "=" + dan*8);
-		System.out.println(dan + "*" + 9 + "=" + dan*9);
-		
+		int dan=7;		
+		for (int i = 1; i <= 9; i++) {
+		    System.out.println(dan + "*" + i + "=" + (dan * i));
+		}
 
 	}
 }
